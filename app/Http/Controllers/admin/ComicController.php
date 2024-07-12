9a9ab5270950d4b8b9a9ecbb0bd12f0fd9fba25b
byHYONS <byhyons@gmail.com>
2024-07-12 22:33:57 +0200

    feat: aggiunto header

--- app/Http/Controllers/admin/ComicController.php
+++ app/Http/Controllers/admin/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -12,7 +13,9 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+
+        return view('comics.index', compact('comics'));
     }
 
     /**
@@ -20,7 +23,7 @@ class ComicController extends Controller
      */
     public function create()
     {
-        //
+        return view('comics.create');
     }
 
     /**
@@ -28,15 +31,33 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+          
+        $data = $request->all();
+
+        $comic = new Comic();
+
+        $comic->title = $data['title'];
+        $comic->description = $data['description'];
+        $comic->thumb = $data['thumb'];
+        $comic->price = $data['price'];
+        $comic->series = $data['series'];
+        $comic->sale_date = $data['sale_date'];
+        $comic->type = $data['type'];
+        $comic->artists = $data['artists'];
+        $comic->writers = $data['writers'];
+
+        $comic->save();
+
+        return redirect()->route('comics.show', $comic->id);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Comic $comics)
     {
-        //
+        return view('comics.show', compact('comics'));
     }
 
     /**
--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-    <h2>sono l'header</h2>
+    <div class="container">
+        <div class="logo">
+            <a href="{{route('comics.index')}}">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="{{route('comics.index')}}">Home</a></li>
+                <li><a href="{{route('comics.index')}}">Comic</a></li>
+                <li><a href="{{route('comics.index')}}">Price</a></li>
+                <li><a href="{{route('comics.index')}}">Contact</a></li>
+            </ul>
+        </nav>
+
+    </div>
 </header>
